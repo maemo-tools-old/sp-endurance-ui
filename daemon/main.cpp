0f9c17747ef80cb4bc4f14d887818e44aaad5b95
Tommi Rantala <ext-tommi.1.rantala@nokia.com>
2011-12-22 10:56:57 +0200

    Remove the EnduranceDaemon prefix from daemon syslog messages

--- daemon/main.cpp
+++ daemon/main.cpp
@@ -38,19 +38,19 @@ int main(int argc, char **argv)
 	QDBusConnection connection = QDBusConnection::systemBus();
 	if (!connection.isConnected()) {
 		syslog(LOG_ERR,
-			"EnduranceDaemon ERROR: DBus connection failed.");
+			"ERROR: DBus connection failed.");
 		return 1;
 	}
 	if (!connection.registerService("com.nokia.EnduranceDaemon")) {
 		syslog(LOG_ERR,
-			"EnduranceDaemon ERROR: unable to register DBus "
+			"ERROR: unable to register DBus "
 			"service 'com.nokia.EnduranceDaemon' (%s).",
 			connection.lastError().message().toAscii().data());
 		return 1;
 	}
 	if (!connection.registerObject("/com/nokia/EnduranceDaemon", &daemon)) {
 		syslog(LOG_ERR,
-			"EnduranceDaemon ERROR: unable to register object "
+			"ERROR: unable to register object "
 			"/com/nokia/EnduranceDaemon.\n");
 		return 1;
 	}
